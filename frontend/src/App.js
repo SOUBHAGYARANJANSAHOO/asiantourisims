2e941d91c62e2f6e15810d7e39c9ab0389b0f8d2
@SOUBHAGYARANJANSAHOO <sahoosoubhagya95@gmail.com>
2021-08-18 13:40:25 +0530

    reset search input upon submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 
 const App = () => {
@@ -10,10 +11,20 @@ const App = () => {
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
+    
     console.log(word);
+    
+    fetch('https://api.unsplash.com/photos/random/?query=${word}&client_id=UHqM9d7lxCbLnnNXdG-yDd1ixjYfCcgSMNJA8wpcxFc')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+    setword('');
   }
   
-
   return (
     <div>
      <Header title="asiantourisims"/>
